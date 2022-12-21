02713429447f919ace776ef29629fc130960823c
Bonus156 <Bonus.by@mail.ru>
2022-12-21 14:31:16 +0300

    feat: add query for sorter and filters

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,6 +1,8 @@
 import { ProductList, Product, GroupeBy } from '../../models/types';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const logo = require('../../assets/icons/shopping-cart.svg');
+const logo = require('../../assets/images/shopping-cart.svg');
+
+const searchParams = new URLSearchParams(document.location.search);
 
 let products: Product[] = [];
 export async function getList() {
@@ -22,7 +24,7 @@ const filtersContainer: HTMLElement = document.createElement('div');
 filtersContainer.className = 'filter';
 
 const filterBrand: HTMLElement = document.createElement('div');
-filterBrand.className = 'filter__brand';
+filterBrand.className = 'filter__brand filter__block';
 const brandHeader: HTMLElement = document.createElement('div');
 brandHeader.className = 'filter__brand-header';
 brandHeader.textContent = 'Brand';
@@ -30,14 +32,45 @@ const brandList: HTMLElement = document.createElement('div');
 brandList.className = 'filter__list';
 
 const filterCategory: HTMLElement = document.createElement('div');
-filterCategory.className = 'filter__category';
+filterCategory.className = 'filter__category filter__block';
 const categoryHeader: HTMLElement = document.createElement('div');
 categoryHeader.className = 'filter__category-header';
 categoryHeader.textContent = 'Category';
 const categoryList: HTMLElement = document.createElement('div');
 categoryList.className = 'filter__list';
 
-filtersContainer.append(filterBrand, filterCategory);
+const filterRanges: HTMLElement = document.createElement('div');
+filterRanges.className = 'filter__ranges filter__block';
+const rangeContainerOne: HTMLElement = document.createElement('div');
+rangeContainerOne.className = 'filter__range range__container';
+const slidersControlOne: HTMLElement = document.createElement('div');
+slidersControlOne.className = 'filter__sliders range__sliders';
+const sliderFirstOne: HTMLInputElement = document.createElement('input');
+const sliderSecondOne: HTMLInputElement = document.createElement('input');
+sliderFirstOne.id = 'fromPrice';
+sliderFirstOne.type = 'range';
+sliderSecondOne.id = 'toPrice';
+sliderSecondOne.type = 'range';
+
+const rangeContainerTwo: HTMLElement = document.createElement('div');
+rangeContainerTwo.className = 'filter__range range__container';
+const slidersControlTwo: HTMLElement = document.createElement('div');
+slidersControlTwo.className = 'filter__sliders range__sliders';
+const sliderFirstTwo: HTMLInputElement = document.createElement('input');
+const sliderSecondTwo: HTMLInputElement = document.createElement('input');
+sliderFirstTwo.id = 'fromStock';
+sliderFirstTwo.type = 'range';
+sliderSecondTwo.id = 'toStock';
+sliderSecondTwo.type = 'range';
+
+filterRanges.append(rangeContainerOne, rangeContainerTwo);
+rangeContainerOne.append(slidersControlOne);
+slidersControlOne.append(sliderFirstOne, sliderSecondOne);
+
+rangeContainerTwo.append(slidersControlTwo);
+slidersControlTwo.append(sliderFirstTwo, sliderSecondTwo);
+
+filtersContainer.append(filterBrand, filterCategory, filterRanges);
 filterBrand.append(brandHeader, brandList);
 filterCategory.append(categoryHeader, categoryList);
 
@@ -53,6 +86,9 @@ for (let i = 0; i < sortMethods.length; i++) {
     sortBtn.type = 'radio';
     sortBtn.name = 'radio';
     sortBtn.id = `sort-${i + 1}`;
+    if (searchParams.get('sort') === `${i + 1}`) {
+        sortBtn.checked = true;
+    }
     sortBtn.value = sortMethods[i];
     labelForSort = document.createElement('label');
     labelForSort.htmlFor = sortBtn.id;
@@ -65,12 +101,20 @@ function sortItems(data: Product[]) {
     const sortMethod: string | undefined = sortAndSettings.querySelector('input:checked')?.id;
     switch (sortMethod) {
         case 'sort-1':
+            searchParams.set('sort', '1');
+            window.history.replaceState(null, '', '?' + searchParams.toString());
             return data.sort((a, b) => a.price - b.price);
         case 'sort-2':
+            searchParams.set('sort', '2');
+            window.history.replaceState(null, '', '?' + searchParams.toString());
             return data.sort((a, b) => b.price - a.price);
         case 'sort-3':
+            searchParams.set('sort', '3');
+            window.history.replaceState(null, '', '?' + searchParams.toString());
             return data.sort((a, b) => a.rating - b.rating);
         case 'sort-4':
+            searchParams.set('sort', '4');
+            window.history.replaceState(null, '', '?' + searchParams.toString());
             return data.sort((a, b) => b.rating - a.rating);
         default:
             return data;
@@ -107,6 +151,7 @@ const checkboxesCategory: HTMLInputElement[] = [];
 
 function fillBrandList(data: GroupeBy) {
     if (!brandList.firstElementChild) {
+        const selectedBrands: string[] | undefined = searchParams.get('brand')?.split('↕');
         for (const brand in data) {
             const checkboxCont: HTMLElement = document.createElement('div');
             checkboxCont.className = 'checkbox-line';
@@ -114,6 +159,9 @@ function fillBrandList(data: GroupeBy) {
             checkboxIn.type = 'checkbox';
             checkboxIn.id = `${brand}`;
             checkboxIn.className = 'checkbox';
+            if (selectedBrands?.includes(brand.toUpperCase())) {
+                checkboxIn.checked = true;
+            }
             checkboxes.push(checkboxIn);
             checkboxesBrand.push(checkboxIn);
             const checkboxLbl: HTMLLabelElement = document.createElement('label');
@@ -135,6 +183,7 @@ function fillBrandList(data: GroupeBy) {
 
 function fillCategoryList(data: GroupeBy) {
     if (!categoryList.firstElementChild) {
+        const selectedCategories: string[] | undefined = searchParams.get('category')?.split('↕');
         for (const category in data) {
             const checkboxCont: HTMLElement = document.createElement('div');
             checkboxCont.className = 'checkbox-line';
@@ -142,6 +191,9 @@ function fillCategoryList(data: GroupeBy) {
             checkboxIn.type = 'checkbox';
             checkboxIn.id = `${category}`;
             checkboxIn.className = 'checkbox';
+            if (selectedCategories?.includes(category.toLowerCase())) {
+                checkboxIn.checked = true;
+            }
             checkboxes.push(checkboxIn);
             checkboxesCategory.push(checkboxIn);
             const checkboxLbl: HTMLLabelElement = document.createElement('label');
@@ -165,20 +217,29 @@ export async function start() {
     await getList().then(getProductsList);
     fillBrandList(groupeByBrand(products));
     fillCategoryList(groupeByCategory(products));
-    createCards(products);
+    getCheckedItems();
 }
 
 let listOfCheckedCheckboxes: HTMLInputElement[][] = [checkboxesBrand, checkboxesCategory];
 
 function getCheckedItems() {
-    // cardsBlock.innerHTML = '';
     listOfCheckedCheckboxes = [checkboxesBrand, checkboxesCategory];
     if (checkboxes.filter((input) => input.checked).length > 0) {
         if (checkboxesBrand.filter((input) => input.checked).length > 0) {
             listOfCheckedCheckboxes[0] = checkboxesBrand.filter((input) => input.checked);
+            let brandList = '';
+            listOfCheckedCheckboxes[0].forEach((el) => (brandList += brandList ? `↕${el.id}` : `${el.id}`));
+            searchParams.set('brand', brandList);
+        } else {
+            searchParams.delete('brand');
         }
         if (checkboxesCategory.filter((input) => input.checked).length > 0) {
             listOfCheckedCheckboxes[1] = checkboxesCategory.filter((input) => input.checked);
+            let categoryList = '';
+            listOfCheckedCheckboxes[1].forEach((el) => (categoryList += categoryList ? `↕${el.id}` : `${el.id}`));
+            searchParams.set('category', categoryList);
+        } else {
+            searchParams.delete('category');
         }
         const prodList: Product[] = products.filter((item) => {
             return (
@@ -186,9 +247,13 @@ function getCheckedItems() {
                 listOfCheckedCheckboxes[1].map((el) => el.id).includes(item.category.toLowerCase())
             );
         });
+        window.history.replaceState(null, '', '?' + searchParams.toString());
         createCards(prodList);
     } else {
         listOfCheckedCheckboxes = [checkboxesBrand, checkboxesCategory];
+        searchParams.delete('brand');
+        searchParams.delete('category');
+        window.history.replaceState(null, '', '?' + searchParams.toString());
         createCards(products);
     }
 }
@@ -225,7 +290,10 @@ export function createCards(data: Product[]) {
         cartIcon.src = logo;
         cartIcon.alt = 'cart';
         cartIcon.className = 'card__cart-icon';
-        addBtn.append(cartIcon);
+        const cartText: HTMLElement = document.createElement('span');
+        cartText.className = 'add-to-cart';
+        cartText.textContent = 'add';
+        addBtn.append(cartIcon, cartText);
         const detailsBtn: HTMLElement = document.createElement('button');
         detailsBtn.className = 'card__detail-btn';
         detailsBtn.textContent = 'details';
